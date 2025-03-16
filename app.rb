9d79d909450624f8c6402a059d81409b9c2de33d
Gami <s1320016@u-aizu.ac.jp>
2025-03-16 19:18:14 +0900

    station08 commit

--- app.rb
+++ app.rb
@@ -9,3 +9,9 @@ get '/todos' do
   @todos = DB.execute('SELECT * FROM todos')
   erb :todos
 end
+
+post '/todos' do
+  DB.execute('INSERT INTO todos (title) VALUES (?)', [params[:title]])
+  @todos = DB.execute('SELECT * FROM todos')
+  erb :todos
+end
