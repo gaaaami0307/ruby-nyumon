8812143d80984c8ff91cf02f092f7571a6766856
Gami <s1320016@u-aizu.ac.jp>
2025-03-16 22:27:26 +0900

    Add post todo api

--- app.rb
+++ app.rb
@@ -24,3 +24,23 @@ put '/todos/:id' do
   DB.execute('UPDATE todos SET title = ? WHERE id = ?',[params[:title],params[:id]])
   redirect '/todos'
 end
+
+delete '/todos/:id' do
+  DB.execute('DELETE FROM todos WHERE id = ?',[params[:id]])
+  redirect '/todos'
+end
+
+get '/api/todos' do
+  content_type :json
+  todos=DB.execute('SELECT * FROM todos')
+  JSON.pretty_generate(todos)
+end
+
+#titleを受け取る
+post '/api/todos' do
+  content_type :json
+  title = params[:title]
+  DB.execute('INSERT INTO todos (title) VALUES (?)',title)
+  response=DB.execute('SELECT * FROM todos WHERE title=?',title).last
+  JSON.pretty_generate(response)
+end
